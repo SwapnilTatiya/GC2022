79ff6b0b504d2c0cbb5045011e55d54bee2832fe
SwapnilTatiya <81140186+SwapnilTatiya@users.noreply.github.com>
2022-03-19 15:33:07 +0530

    DiscussionForumApp

---
+++ LetsDiscuss/app/src/main/java/com/example/letsdiscuss/loginActivity.java
@@ -0,0 +1,21 @@
+package com.example.letsdiscuss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class loginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void goToForgotPassword(View view) {
+    }
+
+    public void loginButtonClicked(View view) {
+    }
+}
